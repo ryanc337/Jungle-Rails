bdc1b0450704e9d80aca55c6a040d2e41596a7e8
Ryan <cordingley.ryan@gmail.com>
2019-12-17 04:52:28 +0000

    added user-specs pt.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,12 @@ class SessionsController < ApplicationController
       redirect_to '/'
     else
       # If user's login doesn't work, send them back to the login form.
-        redirect_to '/login'
-      end
+      redirect_to '/login'
+    end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to '/login'
   end
-
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-
-    # subject { User.create(email: "hello@gmail.com", name: "Tom", password: "password", password_confirmation: "password") }
-    
+    password_confirmation = "password"
+    password = "password"
     it 'should check the db to make sure the email is unqiue' do
       should validate_uniqueness_of(:email)
     end
@@ -13,5 +12,14 @@ RSpec.describe User, type: :model do
       should validate_presence_of(:name)
     end
 
+    it "should ensure that password is same as password_confirmation" do 
+      expect(password).to eq 'password'
+    end
+
+    it "should expect password to have minimum length" do
+      should validate_length_of(:password).
+        is_at_least(5).
+        on(:create)
+    end
   end
 end
